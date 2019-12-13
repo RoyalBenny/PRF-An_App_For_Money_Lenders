84c5f2c0d4424ecefcb8e387ab18fe07e0bd64b4
Royal Benny <45265641+RoyalBenny@users.noreply.github.com>
2019-12-13 15:47:03 +0530

    recent fragment updated

--- app/src/main/java/com/prf/prf/recentFragment.kt
+++ app/src/main/java/com/prf/prf/recentFragment.kt
@@ -9,16 +9,15 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ExpandableListView
 import android.widget.Toast
 import io.reactivex.disposables.Disposable
 import java.util.*
 import kotlin.collections.ArrayList
 
-var adapter2:classAdapter?=null
-
 
 class recentFragment : Fragment() {
-    var subscribe: Disposable?=null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
@@ -42,35 +41,20 @@ class recentFragment : Fragment() {
 
         }
         globalObject.globalList=dataRecentUser
-        adapter=classAdapter( globalObject.globalList!!)
-
-
-
         val rootView=inflater.inflate(R.layout.fragment_recent, container, false)
+//
+//
+//        recyclerView=rootView.findViewById(R.id.recyclerView) as RecyclerView
+//
+//
+//        recyclerView!!.layoutManager= LinearLayoutManager(activity)
+//
+//        recyclerView!!.adapter = adapter
+        val expListView=rootView.findViewById<ExpandableListView>(R.id.expandableRecentListView)
 
-        val recyclerView=rootView.findViewById(R.id.recyclerView_recent) as RecyclerView
+        listAdapter=ExpanadbleListAdapter(con!!,expListView,globalObject.returnGlobals(),1,globalObject.returnGlobals())
 
-
-
-        recyclerView.layoutManager= LinearLayoutManager(activity)
-        recyclerView.adapter = adapter
-
-
-
-
-        subscribe=adapter!!.clickEvent?.subscribe {
-
-            var intent = Intent(con!!, Main2Activity::class.java)
-
-            intent.putExtra("name", it.name)
-            intent.putExtra("index",dataRecentUser.indexOf(it).toString())
-
-            startActivity(intent)
-
-        }
-
-
-
+        expListView.setAdapter(listAdapter)
 
 
 
@@ -79,12 +63,6 @@ class recentFragment : Fragment() {
 
 
         return rootView
-
-
-    }
-    override fun onDestroy() {
-        super.onDestroy()
-        subscribe?.dispose()
 
 
     }
